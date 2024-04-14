783d03cfcb43fa8208e7f4536de0315ceea01012
BlueBallFatBerg <96768062+BlueBallFatBerg@users.noreply.github.com>
2024-04-14 16:18:47 +0800

    fonts_stuff

---
+++ static/js/min-js/min.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+var text = fs.readFileSync('./words', 'utf8')
+// console.log(text)
+
+var Fontmin = require('fontmin');
+
+var fontmin = new Fontmin()
+	.src(['./raw_fonts/Songti*.ttf'])
+	.dest('../../fonts')
+	.use(Fontmin.glyph({
+		text: text,
+		hinting: false,
+	}))
+	.use(Fontmin.ttf2woff2());
+
+fontmin.run(function (err, files) {
+    if (err) {
+        throw err;
+    }
+    console.log(files[0]);
+});
